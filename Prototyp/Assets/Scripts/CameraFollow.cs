9406e6ed2801f1ebcfbbcc47501b94dbd1b595ac
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2016-03-18 13:17:21 +0100

    Prototype added

---
+++ Prototyp/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+	{
+		public Transform myTarget;
+		public float mySmoothing = 5f;
+
+		Vector3 myOffset;
+
+		void Start()
+		{
+			transform.position = new Vector3 (myTarget.transform.position.x, myTarget.transform.position.y, transform.position.z);
+			myOffset = transform.position - myTarget.position;
+		}
+
+		void FixedUpdate()
+		{
+		Vector3 targetCameraPosition = myTarget.position + myOffset;
+			transform.position = Vector3.Lerp(transform.position, targetCameraPosition, mySmoothing * Time.deltaTime);
+		}
+	}
+
---
+++ Prototyp/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour
+{
+	Animator myAnimator;
+
+	void Awake()
+	{
+		myAnimator = GetComponentInParent<Animator>();
+	}
+		
+	void OnTriggerEnter2D(Collider2D aOther)
+	{
+		myAnimator.SetTrigger ("Open");
+	}
+
+	void OnTriggerExit2D(Collider2D aOther)
+	{
+		myAnimator.SetTrigger ("Closed");
+	}
+
+}
