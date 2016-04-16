d117d59c65d9fe7785ccca3f94d4d436a9c1eead
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2016-04-16 23:03:55 +0200

    Bootstrap manager made

---
+++ No Silver Bullet/Assets/Scripts/BootstrapManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BootstrapManager : MonoBehaviour
+{
+	void Update ()
+	{
+		if (GameObject.FindGameObjectWithTag ("Player") && GameObject.FindGameObjectWithTag ("MainCamera"))
+		{
+			Application.LoadLevel ("Scenes/Scene");
+		}
+	}
+}
