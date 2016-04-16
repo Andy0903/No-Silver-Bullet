a1a98c4e2d7e06a2c9ad86c5f6876f41b621abae
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2016-04-16 22:48:55 +0200

    Bootstrap + DoNotDestroy script made

---
+++ No Silver Bullet/Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+	#region private methods
+
+	private void Awake ()
+	{
+		Application.DontDestroyOnLoad (gameObject);
+	}
+
+	#endregion
+
+}
---
+++ No Silver Bullet/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+	void Awake ()
+	{
+		Object.DontDestroyOnLoad (gameObject);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.F))
+		{
+			Application.LoadLevel ("Scenes/Act3");
+		}
+
+		if (Input.GetKeyDown (KeyCode.E))
+		{
+			Application.LoadLevel ("Scenes/Scene");
+		}
+	}
+}
