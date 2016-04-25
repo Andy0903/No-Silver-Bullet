4603d4985cadb357b7579595bfb3020b3de0f926
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2016-04-25 20:15:57 +0200

    Made teleporter script

---
+++ No Silver Bullet/Assets/Scripts/TeleportManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportManager : MonoBehaviour
+{
+	#region Member variables
+
+	public int myTeleportIndex;
+	const float Cooldown = 0.1f;
+	float myTimer;
+
+	#endregion
+
+	#region Private mehtods
+
+	private void Awake ()
+	{
+		myTimer = 0;
+	}
+
+	private void OnTriggerEnter2D (Collider2D aOther)
+	{
+		if (aOther.tag == "Player")
+		{
+			GameObject[] teleports = GameObject.FindGameObjectsWithTag ("Teleport");
+
+			foreach (GameObject teleporter in teleports)
+			{
+				TeleportManager teleportManager = teleporter.GetComponent<TeleportManager> ();
+
+				if (teleportManager.myTeleportIndex == myTeleportIndex && teleporter != gameObject)
+				{
+					if (myTimer <= Time.time)
+					{
+						aOther.transform.position = teleporter.transform.position;
+						myTimer = Time.time + Cooldown;
+						teleportManager.myTimer = myTimer;
+					}
+				}
+			}
+		}
+	}
+
+	#endregion
+}
