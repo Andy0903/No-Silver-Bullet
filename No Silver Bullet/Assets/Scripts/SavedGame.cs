9957efca355756792443664c5928b5bb5ed9223b
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2016-04-26 22:09:59 +0200

    Added Save/Load, XMLfile, progresslog

---
+++ No Silver Bullet/Assets/Scripts/SavedGame.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Xml.Serialization;
+
+public class SavedGame
+{
+	#region Member variables
+
+	const string FilePath = "Assets\\XML\\SaveGame.xml";
+	public float myPlayerX;
+	public float myPlayerY;
+	public ProgressTracker myProgressTracker;
+
+	#endregion
+
+	#region Private methods
+
+	public static void SaveGame ()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		SavedGame save = new SavedGame ();
+		save.myPlayerX = player.transform.position.x;
+		save.myPlayerY = player.transform.position.y;
+		save.myProgressTracker = player.GetComponent<PlayerController> ().ProgressTracker;
+		//TODO inventory
+
+		XmlSerializer serializer = new XmlSerializer (typeof(SavedGame));
+		using (StreamWriter writer = new StreamWriter (FilePath))
+		{
+			serializer.Serialize (writer, save);
+		}
+	}
+
+	public static SavedGame LoadGame ()
+	{
+		SavedGame savedGame;
+
+		XmlSerializer serializer = new XmlSerializer (typeof(SavedGame));
+		using (FileStream filestream = new FileStream (FilePath, FileMode.Open))
+		{
+			savedGame = (SavedGame)serializer.Deserialize (filestream);
+		}
+
+		return savedGame;
+	}
+
+	
+	#endregion
+}
