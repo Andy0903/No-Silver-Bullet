6d676cbefc036aee58598620428cf48115685837
Jesper Vallett <Blueheart_low@hotmail.com>
2016-04-27 11:11:32 +0200

    Cleaned MusicObjectController.cs

--- No Silver Bullet/Assets/Scripts/MusicObjectController.cs
+++ No Silver Bullet/Assets/Scripts/MusicObjectController.cs
@@ -4,11 +4,9 @@ using System.Collections;
 public class MusicObjectController : MonoBehaviour 
 {
 	#region Member variables
-	//public int myRange; //Not used anymore
 	public AudioClip myAudioClip;
 	private AudioClip myPreviousGlobalClip;
 	private BoxCollider2D myBoxCollider;
-	private GameObject myTarget;
 	private bool mySwappedMusic = false;
 	#endregion
 
@@ -16,25 +14,7 @@ public class MusicObjectController : MonoBehaviour
 	private void Awake () 
 	{
 		myBoxCollider = GetComponent<BoxCollider2D> ();
-		myTarget = GameObject.FindGameObjectWithTag ("Player"); //Sets the target
 	}
-
-	/*private void Update () 
-	{
-		float distanceFromTarget = Vector3.Distance (transform.position, myTarget.transform.position);
-
-		if (distanceFromTarget < myRange && mySwappedMusic == false)
-		{
-			myPreviousGlobalClip = SoundManager.instance.myMusicSource.clip;
-			SoundManager.instance.ChangeBGMusic (myAudioClip);
-			mySwappedMusic = true;
-		}
-		else if (distanceFromTarget > myRange && mySwappedMusic == true)
-		{
-			SoundManager.instance.ChangeBGMusic (myPreviousGlobalClip);
-			mySwappedMusic = false;
-		}
-	}*/
 
 	private void OnTriggerEnter2D(Collider2D aCollider)
 	{
