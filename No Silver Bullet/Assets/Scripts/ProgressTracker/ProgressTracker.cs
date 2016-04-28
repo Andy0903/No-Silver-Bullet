8a258aa5dba67547fc683f6c327dff7a1364735d
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2016-04-28 22:28:33 +0200

    Missed file for some reason

---
+++ No Silver Bullet/Assets/Scripts/ProgressTracker/ProgressTracker.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProgressTracker
+{
+	#region Member variables
+
+	public enum Quests
+	{
+		DefeatAct1Boss,
+		DefeatAct2Boss,
+		DefeatAct3Boss,
+		DefeatAct4Boss,
+		DefeatAct5Boss,
+	}
+
+	public SerializableDictionary<Quests, bool> myProgress;
+
+	#endregion
+
+	#region Private methods
+
+	public ProgressTracker ()
+	{
+		myProgress = new SerializableDictionary<Quests, bool> ();
+	}
+
+	public bool GetQuestStatus (Quests aQuest)
+	{
+		bool value;
+		myProgress.TryGetValue (aQuest, out value);
+		return value;
+	}
+
+	public void SetQuestStatus (Quests aQuest, bool aState)
+	{
+		myProgress [aQuest] = aState;
+		SavedGame.SaveGame ();
+	}
+
+	#endregion
+
+}
