debfa27a8b52384be27ce81af3d20f14eeed4696
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2016-05-02 20:00:41 +0200

    Remade inventory, drag n drop fixed. Stats and such not yet in game

---
+++ No Silver Bullet/Assets/InventorySlot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+	#region Properties
+
+	public GameObject Item
+	{
+		get
+		{
+			if (gameObject.transform.childCount > 0)
+			{
+				return transform.GetChild (0).gameObject;
+			}
+			return null;
+		}
+	}
+
+	#endregion
+
+	#region Public methods
+
+	public void OnDrop (PointerEventData aEventData)
+	{
+		if (Item == null)
+		{
+			DragHandler.myItemBeingDragged.transform.SetParent (gameObject.transform);
+			ExecuteEvents.ExecuteHierarchy<IHasChanged> (gameObject, null, (x, y) => x.HasChanged ());
+		}
+	}
+
+	#endregion
+}
