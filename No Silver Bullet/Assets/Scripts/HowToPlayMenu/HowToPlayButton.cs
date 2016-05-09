b6802d516203e0017733271e109a92a114b1f4bc
Andreas Gustafsson <andreasgustafsson95@gmail.com>
2016-05-09 17:47:26 +0200

    Made HowToPlay menu and buttons. Fixed resultion on MainMenu + GUI ingame. Now correctly scaling with all resolutions.

---
+++ No Silver Bullet/Assets/Scripts/HowToPlayMenu/HowToPlayButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HowToPlayButton : MenuButtonClick
+{
+	#region Events
+
+	public delegate void ClickAction ();
+
+	public static event ClickAction OnClicked;
+
+	#endregion
+
+	#region Public methods
+
+	public override void OnClick ()
+	{
+		OnClicked ();
+	}
+
+	#endregion
+}
---
+++ No Silver Bullet/Assets/Scripts/HowToPlayMenu/HowToPlayManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HowToPlayManager : MonoBehaviour
+{
+	#region Member variables
+
+	public GameObject myCanvas;
+
+	#endregion
+
+	#region Properties
+
+	public bool InHowToPlay
+	{
+		get;
+		private set;
+	}
+
+	#endregion
+
+	#region Private methods
+
+	void OnEnable ()
+	{
+		HowToPlayButton.OnClicked += ShiftHowToPlayState;
+	}
+
+
+	void OnDisable ()
+	{
+		HowToPlayButton.OnClicked -= ShiftHowToPlayState;
+	}
+
+	private void ShiftHowToPlayState ()
+	{
+		InHowToPlay = !InHowToPlay;
+	}
+
+	private void Start ()
+	{
+		InHowToPlay = false;
+		myCanvas.SetActive (InHowToPlay);
+	}
+
+	private void Update ()
+	{
+		myCanvas.SetActive (InHowToPlay);
+	}
+
+	#endregion
+
+}
